c2b8047b368efd1e09e6a76ca4cca8b0e681c4c6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-25 00:32:29 +0900

    BarrageSpawnDataクラスとそのメタファイルを追加

---
+++ SoulSTG/Assets/SoulSTG/Scripts/BarrageSystems/BarrageSpawnData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace SoulSTG.BarrageSystems
+{
+    [CreateAssetMenu(fileName = "BarrageSpawnData", menuName = "ScriptableObjects/BarrageSpawnData")]
+    public class BarrageSpawnData : ScriptableObject
+    {
+    }
+}
