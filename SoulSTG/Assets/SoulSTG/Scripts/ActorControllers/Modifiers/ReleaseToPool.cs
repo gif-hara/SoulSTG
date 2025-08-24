f70e0cd28f870c28748ed8266f4af1159703e8d6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-24 22:21:58 +0900

    ReleaseToPoolクラスを新規作成し、アクターをプールに戻す機能を追加

---
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Modifiers/ReleaseToPool.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+
+namespace SoulSTG.ActorControllers.Modifiers
+{
+    public sealed class ReleaseToPool : IActorModifier
+    {
+        public UniTask InvokeAsync(Actor actor, Container container, CancellationToken cancellationToken)
+        {
+            TinyServiceLocator.Resolve<GameObjectPool>().Release(actor);
+            return UniTask.CompletedTask;
+        }
+    }
+}
