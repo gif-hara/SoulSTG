cad8d390ab88c3e521a0b847b56ac31deb264f18
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-24 21:29:42 +0900

    GameObjectPoolクラスを新規作成し、オブジェクトプール機能を実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/GameObjectPool.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Pool;
+
+namespace SoulSTG
+{
+    public class GameObjectPool
+    {
+        private readonly Dictionary<GameObject, CancellationTokenSource> lifeTimeTokens = new();
+
+        private readonly Dictionary<GameObject, ObjectPool<GameObject>> pools = new();
+
+        public T Rent<T>(T prefab) where T : Component
+        {
+            if (!pools.TryGetValue(prefab.gameObject, out var pool))
+            {
+                pool = new ObjectPool<GameObject>(
+                    createFunc: () => Object.Instantiate(prefab.gameObject),
+                    actionOnGet: go =>
+                    {
+                        Assert.IsNull(lifeTimeTokens[go]);
+                        lifeTimeTokens[go] = new CancellationTokenSource();
+                        go.SetActive(true);
+                    },
+                    actionOnRelease: go =>
+                    {
+                        Assert.IsNotNull(lifeTimeTokens[go]);
+                        lifeTimeTokens[go].Cancel();
+                        lifeTimeTokens[go].Dispose();
+                        lifeTimeTokens[go] = null;
+                        go.SetActive(false);
+                    },
+                    actionOnDestroy: go =>
+                    {
+                        Assert.IsNotNull(lifeTimeTokens[go]);
+                        lifeTimeTokens[go].Cancel();
+                        lifeTimeTokens[go].Dispose();
+                        lifeTimeTokens[go] = null;
+                        Object.Destroy(go);
+                    },
+                    collectionCheck: false,
+                    defaultCapacity: 10
+                    );
+                pools[prefab.gameObject] = pool;
+            }
+
+            var obj = pool.Get();
+            return obj.GetComponent<T>();
+        }
+    }
+}
