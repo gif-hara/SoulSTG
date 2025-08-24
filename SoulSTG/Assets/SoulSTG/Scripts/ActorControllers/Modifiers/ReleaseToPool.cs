efb6d30b763404c2bd543c76786f9cf3e68ab9e8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-24 22:34:28 +0900

    ReleaseToPoolクラスにSerializable属性を追加

--- SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Modifiers/ReleaseToPool.cs
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Modifiers/ReleaseToPool.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 
 namespace SoulSTG.ActorControllers.Modifiers
 {
+    [Serializable]
     public sealed class ReleaseToPool : IActorModifier
     {
         public UniTask InvokeAsync(Actor actor, Container container, CancellationToken cancellationToken)
