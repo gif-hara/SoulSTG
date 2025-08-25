3a7f89e94a967a9ac4e2c9198617542517fb5811
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-25 23:57:04 +0900

    Forgetクラスを追加し、弾幕修飾子の非同期処理を実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/BarrageSystems/Modifiers/Forget.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using SoulSTG.ActorControllers;
+using TNRD;
+using UnityEngine;
+
+namespace SoulSTG.BarrageSystems.Modifiers
+{
+    public sealed class Forget : IBarrageModifier
+    {
+        [field: SerializeField]
+        private SerializableInterface<IBarrageModifier>[] modifiers;
+
+        public UniTask InvokeAsync(Actor owner, Transform spawnPoint, CancellationToken cancellationToken)
+        {
+            foreach (var modifier in modifiers)
+            {
+                modifier.Value.InvokeAsync(owner, spawnPoint, cancellationToken).Forget();
+            }
+            return UniTask.CompletedTask;
+        }
+    }
+}
