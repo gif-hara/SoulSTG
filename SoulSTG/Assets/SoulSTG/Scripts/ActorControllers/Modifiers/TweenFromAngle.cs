9834048873adf9a845dbabf9e6ee18bdcb164a96
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-25 23:41:25 +0900

    TweenFromAngleクラスを追加し、ActorModifierとしての機能を実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Modifiers/TweenFromAngle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using LitMotion;
+using LitMotion.Extensions;
+using UnityEngine;
+
+namespace SoulSTG.ActorControllers.Modifiers
+{
+    [Serializable]
+    public sealed class TweenFromAngle : IActorModifier
+    {
+        [field: SerializeField]
+        private float distance;
+
+        [field: SerializeField]
+        private float fixedAngle;
+
+        [field: SerializeField]
+        private float seconds;
+
+        [field: SerializeField]
+        private Ease ease;
+
+        public UniTask InvokeAsync(Actor owner, Actor spawnedActor, CancellationToken cancellationToken)
+        {
+            var angle = Quaternion.Euler(0, 0, fixedAngle);
+            var to = spawnedActor.transform.position + angle * spawnedActor.transform.up * distance;
+            return LMotion.Create(spawnedActor.transform.position, to, seconds)
+                .WithEase(ease)
+                .BindToPosition(spawnedActor.transform)
+                .ToUniTask(cancellationToken);
+        }
+    }
+}
