17270f58271dfd2918f51effd8570c3d39102128
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-25 23:51:12 +0900

    クールタイムの管理を追加し、弾発射の条件を改善

--- SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Abilities/ActorBulletSystem.cs
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Abilities/ActorBulletSystem.cs
@@ -1,5 +1,7 @@
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
+using R3.Triggers;
 using SoulSTG.BarrageSystems;
 
 namespace SoulSTG.ActorControllers.Abilities
@@ -10,15 +12,35 @@ namespace SoulSTG.ActorControllers.Abilities
 
         public BarrageSpawnData BarrageSpawnData;
 
+        private float coolTime;
+
         public void Activate(Actor actor)
         {
             this.actor = actor;
+            this.actor.UpdateAsObservable()
+                .Subscribe(this, (_, @this) =>
+                {
+                    if (@this.coolTime > 0)
+                    {
+                        @this.coolTime -= UnityEngine.Time.deltaTime;
+                    }
+                })
+                .RegisterTo(actor.GetLifeTimeToken());
         }
 
         public bool TryFire()
         {
+            if (coolTime > 0)
+            {
+                return false;
+            }
             BarrageSpawnData.SpawnAsync(actor).Forget();
             return true;
+        }
+
+        public void SetCoolTime(float coolTime)
+        {
+            this.coolTime = coolTime;
         }
     }
 }
