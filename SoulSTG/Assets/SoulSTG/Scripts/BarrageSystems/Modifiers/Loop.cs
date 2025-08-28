9929820883cfdb4636498450284bd03c78611c5c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-28 23:16:52 +0900

    Loopクラスを追加し、非同期処理を実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/BarrageSystems/Modifiers/Loop.cs
@@ -0,0 +1,32 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using SoulSTG.ActorControllers;
+using TNRD;
+using UnityEngine;
+
+namespace SoulSTG.BarrageSystems.Modifiers
+{
+    public sealed class Loop : IBarrageModifier
+    {
+        [field: SerializeField, ClassesOnly]
+        private int count;
+
+        [field: SerializeField, ClassesOnly]
+        private SerializableInterface<IBarrageModifier>[] modifiers;
+
+        public async UniTask InvokeAsync(Actor owner, Transform spawnPoint, CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                foreach (var modifier in modifiers)
+                {
+                    await modifier.Value.InvokeAsync(owner, spawnPoint, cancellationToken);
+                }
+            }
+        }
+    }
+}
