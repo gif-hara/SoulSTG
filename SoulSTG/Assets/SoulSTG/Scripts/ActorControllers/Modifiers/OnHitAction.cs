5d9cbba18b443504149a9a1f343d70c8cd783ed6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-08-28 23:57:35 +0900

    OnHitActionクラスを追加し、衝突時のアクションを実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/Modifiers/OnHitAction.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using R3;
+using R3.Triggers;
+using UnityEngine;
+
+namespace SoulSTG.ActorControllers.Modifiers
+{
+    [Serializable]
+    public sealed class OnHitAction : IActorModifier
+    {
+        public UniTask InvokeAsync(Actor owner, Actor spawnedActor, CancellationToken cancellationToken)
+        {
+            owner.OnTriggerEnter2DAsObservable()
+                .Subscribe(collision =>
+                {
+                    Debug.Log("OnHitAction Triggered");
+                })
+                .RegisterTo(cancellationToken);
+            return UniTask.CompletedTask;
+        }
+    }
+}
