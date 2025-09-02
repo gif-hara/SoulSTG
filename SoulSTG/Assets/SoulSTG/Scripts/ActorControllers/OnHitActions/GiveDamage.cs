6ca0174f1cd3487c9cc98936771596a7c9e5bc09
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-02 13:01:16 +0900

    GiveDamageクラスを新規作成し、ダメージ処理を実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/OnHitActions/GiveDamage.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace SoulSTG.ActorControllers.OnHitActions
+{
+    public class GiveDamage : IOnHitAction
+    {
+        [field: SerializeField]
+        private float power;
+
+        public void Invoke(Actor owner, Actor bulletActor, Actor hitActor)
+        {
+            hitActor.GetAbility<Abilities.ActorStatus>().TakeDamage(power);
+        }
+    }
+}
