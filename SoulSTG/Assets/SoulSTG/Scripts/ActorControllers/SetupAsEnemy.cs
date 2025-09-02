580fde6346bc24eff4e175489d479fb0850ff1d6
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-02 15:49:24 +0900

    OnDieActionsのリストを追加し、死亡時のアクションを非同期で呼び出す処理を実装

--- SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/SetupAsEnemy.cs
+++ SoulSTG/Assets/SoulSTG/Scripts/ActorControllers/SetupAsEnemy.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using HK;
+using R3;
 using SoulSTG.ActorControllers.Abilities;
+using SoulSTG.ActorControllers.OnDieActions;
 using SoulSTG.MasterDataSystem;
+using TNRD;
 using UnityEngine;
+using VitalRouter.R3;
 
 namespace SoulSTG.ActorControllers
 {
@@ -13,9 +20,29 @@ namespace SoulSTG.ActorControllers
         [field: SerializeField]
         private string actorSpecId;
 
+        [field: SerializeField, ClassesOnly]
+        private List<SerializableInterface<IOnDieAction>> onDieActions;
+
         void Start()
         {
             actor.AddAbility(new ActorStatus(TinyServiceLocator.Resolve<MasterData>().ActorSpecs.Get(actorSpecId)));
+            actor.Event.Router.AsObservable<ActorEvent.OnDie>()
+                .Subscribe((this, actor), static (evt, t) =>
+                {
+                    var (@this, actor) = t;
+                    @this.InvokeOnDieActionsAsync(actor, actor.GetLifeTimeToken()).Forget();
+                }
+            )
+            .RegisterTo(actor.GetLifeTimeToken());
+        }
+
+        private async UniTask InvokeOnDieActionsAsync(Actor actor, CancellationToken cancellationToken)
+        {
+            foreach (var action in onDieActions)
+            {
+                await action.Value.InvokeAsync(actor, cancellationToken);
+            }
+
         }
 
 #if UNITY_EDITOR
