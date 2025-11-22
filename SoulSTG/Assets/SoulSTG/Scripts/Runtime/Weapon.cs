4795e7be8c59b05cd68f32b9d4a0214969bef621
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-11-22 23:37:47 +0900

    WeaponクラスにSceneViewのローカル位置、回転、スケールを追加

--- SoulSTG/Assets/SoulSTG/Scripts/Runtime/Weapon.cs
+++ SoulSTG/Assets/SoulSTG/Scripts/Runtime/Weapon.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using SoulSTG.ActorControllers;
+using SoulSTG.ActorControllers.Abilities;
 using UnityEngine;
 
 namespace SoulSTG.WeaponControllers
@@ -18,10 +19,29 @@ namespace SoulSTG.WeaponControllers
 
             [field: SerializeField]
             public Transform SceneView { get; private set; }
+
+            [field: SerializeField]
+            public Vector3 LocalPosition { get; private set; }
+
+            [field: SerializeField]
+            public Quaternion LocalRotation { get; private set; }
+
+            [field: SerializeField]
+            public Vector3 LocalScale { get; private set; } = Vector3.one;
         }
 
         public void Activate(Actor actor)
         {
+            var locatorHolder = actor.GetAbility<SceneViewController>().SceneView.LocatorHolder;
+
+            foreach (var sceneView in sceneViews)
+            {
+                var locator = locatorHolder.Get(sceneView.LocatorName);
+                sceneView.SceneView.SetParent(locator, false);
+                sceneView.SceneView.localPosition = sceneView.LocalPosition;
+                sceneView.SceneView.localRotation = sceneView.LocalRotation;
+                sceneView.SceneView.localScale = sceneView.LocalScale;
+            }
         }
     }
 }
