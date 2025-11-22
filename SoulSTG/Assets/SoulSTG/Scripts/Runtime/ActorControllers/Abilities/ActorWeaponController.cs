18d8c5f1573f43ba6c4d8a41c28a20cbc4e9651b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-11-22 18:57:41 +0900

    フォルダ移動

---
+++ SoulSTG/Assets/SoulSTG/Scripts/Runtime/ActorControllers/Abilities/ActorWeaponController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace SoulSTG.ActorControllers.Abilities
+{
+    public sealed class ActorWeaponController : IActorAbility
+    {
+        private Actor actor;
+
+        private readonly HashSet<string> blockedTags = new();
+
+        public void Activate(Actor actor)
+        {
+            this.actor = actor;
+        }
+
+        public bool TryAttack()
+        {
+            if (blockedTags.Count > 0)
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+
+        public void AddBlockedTag(string tag)
+        {
+            blockedTags.Add(tag);
+        }
+
+        public void RemoveBlockedTag(string tag)
+        {
+            blockedTags.Remove(tag);
+        }
+    }
+}
