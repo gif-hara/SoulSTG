4512c79113534166e1aa3eb9434976a1c9b9a077
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-11-22 18:53:25 +0900

    MessageBrokerクラスを新規作成し、IMessagePublisherおよびIMessageReceiverを実装

---
+++ SoulSTG/Assets/SoulSTG/Scripts/MessageBroker.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using R3;
+
+namespace HK
+{
+    public interface IMessagePublisher
+    {
+        void Publish<T>(T message);
+    }
+
+    public interface IMessageReceiver
+    {
+        Observable<T> Receive<T>();
+    }
+
+    public interface IMessageBroker : IMessagePublisher, IMessageReceiver
+    {
+    }
+
+    public class MessageBroker : IMessageBroker, IDisposable
+    {
+        public static readonly IMessageBroker Default = new MessageBroker();
+
+        private bool isDisposed = false;
+
+        private readonly Dictionary<Type, object> notifiers = new();
+
+        public void Publish<T>(T message)
+        {
+            object notifier;
+            lock (notifiers)
+            {
+                if (isDisposed)
+                {
+                    return;
+                }
+
+                if (!notifiers.TryGetValue(typeof(T), out notifier))
+                {
+                    return;
+                }
+            }
+            ((ISubject<T>)notifier).OnNext(message);
+        }
+
+        public Observable<T> Receive<T>()
+        {
+            object notifier;
+            lock (notifiers)
+            {
+                if (isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MessageBroker));
+                }
+
+                if (!notifiers.TryGetValue(typeof(T), out notifier))
+                {
+                    Observable<T> n = new Subject<T>().Synchronize();
+                    notifier = n;
+                    notifiers.Add(typeof(T), notifier);
+                }
+            }
+            return (Observable<T>)notifier;
+        }
+
+        public void Dispose()
+        {
+            lock (notifiers)
+            {
+                if (!isDisposed)
+                {
+                    isDisposed = true;
+                    notifiers.Clear();
+                }
+            }
+        }
+    }
+}
