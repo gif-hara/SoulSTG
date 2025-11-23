8a12e49fb5d1c1b91e69cca4bd2e839495c49880
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-11-23 20:49:30 +0900

    IActionインターフェースにInvokeAsyncメソッドを追加

--- SoulSTG/Assets/SoulSTG/Scripts/Runtime/ActorControllers/Actions/IAction.cs
+++ SoulSTG/Assets/SoulSTG/Scripts/Runtime/ActorControllers/Actions/IAction.cs
@@ -1,6 +1,10 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
 namespace SoulSTG.ActorControllers.Actions
 {
     public interface IAction
     {
+        UniTask InvokeAsync(Actor actor, CancellationToken cancellationToken);
     }
 }
